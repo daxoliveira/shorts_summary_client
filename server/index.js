a5e5ea58dd7c8c14d6aaa1c145f2aeb5290fe379
Dax Oliveira <dax.oliveira.br@gmail.com>
2023-09-20 10:20:24 -0500

    change get method response format to JSON

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ app.use(cors())
 
 app.get("/summary/:id", (request, response) => {
   download(request.params.id)
-  response.send("Video id: " + request.params.id)
+
+  response.json({ result: "Video was successfully downloaded!" })
 })
 
 app.listen(3333, () => console.log("Server running on port 3333!"))
