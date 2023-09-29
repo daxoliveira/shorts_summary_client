73f06247dfef579c0f21a2bd08d4081455bc5e76
Dax Oliveira <dax.oliveira.br@gmail.com>
2023-09-29 08:58:48 -0500

    enable CORS on server so client can access it

--- server/index.js
+++ server/index.js
@@ -10,6 +10,19 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://shorts-summary-ai.netlify.app"
+  )
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World!" })
 })
