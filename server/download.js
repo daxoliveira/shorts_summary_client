7b992bbf7cadbd74ea6f28b71028cbd7819629a9
Dax Oliveira <dax.oliveira.br@gmail.com>
2023-09-29 16:00:03 -0500

    refactor download.js file

--- server/download.js
+++ server/download.js
@@ -1,17 +1,26 @@
 import ytdl from "ytdl-core"
 import fs from "fs"
 
+// Before calling ytdl, ensure the 'tmp' directory exists
+if (!fs.existsSync("./tmp")) {
+  fs.mkdirSync("./tmp")
+}
+
 export const download = (videoId) =>
   new Promise((resolve, reject) => {
     const videoUrl = "https://www.youtube.com/shorts/" + videoId
+    const filePath = "./tmp/audio.mp4"
     console.log("Downloading video..." + videoId)
+
+    const writeStream = fs.createWriteStream(filePath)
 
     ytdl(videoUrl, { quality: "lowestaudio", filter: "audioonly" })
       .on("info", (info) => {
         const seconds = info.formats[0].approxDurationMs / 1000
 
         if (seconds > 60) {
-          throw new Error("Video is too long. Max length is 60 seconds.")
+          console.error("Video is too long. Max length is 60 seconds.")
+          reject(new Error("Video is too long. Max length is 60 seconds."))
         }
       })
       .on("end", () => {
@@ -19,8 +28,38 @@ export const download = (videoId) =>
         resolve()
       })
       .on("error", (error) => {
-        console.log("Error: " + error)
+        console.error("Error: " + error)
         reject(error)
       })
-      .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+      .pipe(writeStream)
+
+    // Handle any writeStream errors
+    writeStream.on("error", (error) => {
+      console.error("WriteStream error: " + error)
+      reject(error)
+    })
   })
+
+// export const download = (videoId) =>
+//   new Promise((resolve, reject) => {
+//     const videoUrl = "https://www.youtube.com/shorts/" + videoId
+//     console.log("Downloading video..." + videoId)
+
+//     ytdl(videoUrl, { quality: "lowestaudio", filter: "audioonly" })
+//       .on("info", (info) => {
+//         const seconds = info.formats[0].approxDurationMs / 1000
+
+//         if (seconds > 60) {
+//           throw new Error("Video is too long. Max length is 60 seconds.")
+//         }
+//       })
+//       .on("end", () => {
+//         console.log("Download finished.")
+//         resolve()
+//       })
+//       .on("error", (error) => {
+//         console.log("Error: " + error)
+//         reject(error)
+//       })
+//       .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+//   })
