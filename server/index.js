b93bc3d1d285e14296b913648f6a1dfb1b8f8f9b
Dax Oliveira <dax.oliveira.br@gmail.com>
2023-10-14 21:37:08 -0500

    test different cors setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-import cors from "cors"
-import express from "express"
+// import cors from "cors"
+// import express from "express"
+const express = require("express")
+const cors = require("cors")
 
 import { download } from "./download.js"
 import { transcribe } from "./transcribe.js"
@@ -10,25 +12,25 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://shorts-summary-ai.netlify.app"
-  )
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  )
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  )
-  res.setHeader("Access-Control-Allow-Credentials", true)
-  res.setHeader("Access-Control-Allow-Private-Network", true)
-  res.setHeader("Access-Control-Max-Age", 7200)
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://shorts-summary-ai.netlify.app"
+//   )
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+//   )
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+//   )
+//   res.setHeader("Access-Control-Allow-Credentials", true)
+//   res.setHeader("Access-Control-Allow-Private-Network", true)
+//   res.setHeader("Access-Control-Max-Age", 7200)
 
-  next()
-})
+//   next()
+// })
 
 // app.use((req, res, next) => {
 //   res.header(
