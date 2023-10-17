c9fdefc37c21c0e3325622c0c13040c8cdae8efb
Dax Oliveira <dax.oliveira.br@gmail.com>
2023-10-17 10:09:17 -0500

    clean up index.js from server

--- server/index.js
+++ server/index.js
@@ -8,56 +8,20 @@ import { convert } from "./convert.js"
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+)
 
-app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
-  return res.json({ message: "Hello World!" })
-  next()
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World!" })
 })
 
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://shorts-summary-ai.netlify.app"
-//   )
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//   )
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   )
-//   res.setHeader("Access-Control-Allow-Credentials", true)
-//   res.setHeader("Access-Control-Allow-Private-Network", true)
-//   res.setHeader("Access-Control-Max-Age", 7200)
-
-//   next()
-// })
-
-// app.use((req, res, next) => {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://shorts-summary-ai.netlify.app"
-//   )
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   )
-//   next()
-// })
-
-// app.get("/", (request, response) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   return response.json({ message: "Hello World!" })
-// })
-
 app.get("/summary/:id", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
   try {
     await download(req.params.id)
     const audioConverted = await convert()
@@ -71,8 +35,6 @@ app.get("/summary/:id", async (req, res) => {
 })
 
 app.post("/summary", async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
   try {
     const result = await summarize(req.body.text)
     return res.json({ result })
