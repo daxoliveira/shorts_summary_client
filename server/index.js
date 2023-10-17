48ed928cbea815b37dc91fe614f66ad8c02c8d55
Dax Oliveira <dax.oliveira.br@gmail.com>
2023-10-17 10:27:06 -0500

    trimming down the cors defaults

--- server/index.js
+++ server/index.js
@@ -11,8 +11,6 @@ app.use(express.json())
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 )
